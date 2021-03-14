c37511062ea3a5c0241eca321c517c38528ccdb0
Lukas Cizek <globus303@gmail.com>
2021-03-14 22:05:42 +0100

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -3,7 +3,9 @@ import NextI18Next from 'next-i18next';
 const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'en',
   defaultNS: 'common',
+  fallbackLng: 'en',
   otherLanguages: [],
+  localePath: typeof window === 'undefined' ? 'public/locales' : 'locales',
 });
 
 export const { appWithTranslation, useTranslation } = NextI18NextInstance;
